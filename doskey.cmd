926a4dd037ef2d3ce1e070cc93cb4167cc1fd059
weightofcode <czorile@gmail.com>
2026-04-14 20:30:39 +0300

    fix: update git commit cmd. add git push cmd

--- doskey.cmd
+++ doskey.cmd
@@ -1,11 +1,10 @@
 @echo off
 @REM Permanent DOSKEY commands for CMD
 @REM Win11 Terminal json file: add the file as a parameter to CMD call
-@REM cmd.exe /K path\doskey.cmd
+@REM cmd.exe /K path\\doskey.cmd
 
 @REM Temp Commented
 @REM doskey gc=git commit -am "$*"
-
 doskey vi=vim
 doskey em=emacs -nw
 doskey np=notepad
@@ -16,3 +15,5 @@ doskey gs=git status
 doskey gd=git diff
 doskey gb=git branch -va
 doskey ga=git add *
+doskey gc=git commit -a -m "$*"
+doskey gp=git push
