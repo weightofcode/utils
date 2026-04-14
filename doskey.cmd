a578e81ca16a7689b4aaa062717604198a3287d4
Arcane Pixel <17513553+weightofcode@users.noreply.github.com>
2026-04-14 10:47:10 +0300

    Add doskey commands for common utilities

---
+++ doskey.cmd
@@ -0,0 +1,10 @@
+@echo off
+doskey vi=vim
+doskey em=emacs -nw
+doskey np=notepad
+doskey ls=dir
+doskey lsd=dir /d
+doskey lsw=dir /w
+doskey gs=git status
+doskey gb=git branch -va
+doskey ga=git add *
