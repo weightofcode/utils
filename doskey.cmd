81fba3d9d85d33d3c8c4a584e6dbb566aa200d1e
Arcane Pixel <17513553+weightofcode@users.noreply.github.com>
2026-04-23 10:45:11 +0300

    feat: add python checker aliases

--- doskey.cmd
+++ doskey.cmd
@@ -5,18 +5,21 @@
 
 @REM Temp Commented
 
+@REM Editors
 doskey vi=vim
 doskey em=emacs -nw
 doskey np=notepad
 
+@REM Shell
 doskey ls=dir
 doskey lsd=dir /d
 doskey lsw=dir /w
-doskey md=mkdir "$*"
 doskey rds=rd /s /q "$*"
+doskey md=mkdir "$*"
 doskey mv=move "$*" "$*"
 doskey cp=copy "$*" "$*"
 
+@REM Git
 doskey ga=git add *
 doskey gb=git branch -va
 doskey gcl=git clone $*
@@ -24,8 +27,13 @@ doskey gclr=git clone --recurse-submodules "$*"
 doskey gcm=git commit -a -m "$*"
 doskey gd=git diff
 doskey gf=git fetch
-doskey gl=git log -n7 --oneline
+doskey gl=git log --oneline -n10 
+doskey glg=git log --all --oneline --graph --decorate -n10
 doskey gpl=git pull
 doskey gps=git push
 doskey grh=git reset --hard HEAD
 doskey gs=git status
+
+@REM Python
+doskey pce=python -m py_compile "$*"  :: errors
+doskey pcs=python -m pyflakes "$*"	:: style (ruff works too)
